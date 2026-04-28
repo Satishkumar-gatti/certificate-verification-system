84aad791b299b517d745f77bbe33b748378325b8
Satishkumargatti <satishkumargatti6@gmail.com>
2026-04-28 22:11:13 +0530

    Update settings.py

--- project/certificates/certificates/settings.py
+++ project/certificates/certificates/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--9us0msf+fw+c%fhib2tjnr9dj^_($-a4!*nqn5@_2!@z8gth2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 # Application definition
 
